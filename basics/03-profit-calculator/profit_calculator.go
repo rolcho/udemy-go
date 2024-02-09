8767908b6765c3152f16eea6c72e4f5b15d40287
rolcho <roland@cimem.hu>
2024-02-09 19:42:39 +0000

    function parameters and return values

--- basics/03-profit-calculator/profit_calculator.go
+++ basics/03-profit-calculator/profit_calculator.go
@@ -16,12 +16,22 @@ func main() {
 	fmt.Print("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
+	e, p, r := profitCalculator(revenue, expenses, taxRate)
+	t := printProfitResult(e, p, r)
+
+	fmt.Print(t)
+}
+
+func profitCalculator(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (100 - taxRate) / 100
 	ratio := ebt / profit
+	return ebt, profit, ratio
+}
 
+func printProfitResult(ebt, profit, ratio float64) string {
 	formattedEbt := fmt.Sprintf("EBT: %v\n", ebt)
 	formattedProfit := fmt.Sprintf("Profit: %v\n", profit)
 	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", ratio)
-	fmt.Print(formattedEbt, formattedProfit, formattedRatio)
+	return formattedEbt + formattedProfit + formattedRatio
 }
