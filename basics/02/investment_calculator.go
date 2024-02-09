ccea97acb19673ab2e0d4d6cacf48b6b7f31e3df
rolcho <roland@cimem.hu>
2024-02-09 17:11:11 +0000

    basic user input

--- basics/02/investment_calculator.go
+++ basics/02/investment_calculator.go
@@ -6,8 +6,18 @@ import (
 )
 
 func main() {
-	expectedReturnRate, investmentAmount, years := 5.5, 1000.0, 10.0
+	const inflationrate = 2.5
+	var expectedReturnRate, investmentAmount, years float64
+
+	fmt.Println("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Println("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Println("Enter the number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationrate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
