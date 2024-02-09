7fbfc80c5b3cf8e19c81ce376d02a41964be8299
rolcho <roland@cimem.hu>
2024-02-09 17:33:41 +0000

    profit calculation practice

--- basics/02-investment-calculator/investment_calculator.go
+++ basics/02-investment-calculator/investment_calculator.go
@@ -9,15 +9,18 @@ func main() {
 	const inflationrate = 2.5
 	var expectedReturnRate, investmentAmount, years float64
 
-	fmt.Println("Enter the investment amount: ")
+	fmt.Print("Enter the investment amount: ")
 	fmt.Scan(&investmentAmount)
-	fmt.Println("Enter the expected return rate: ")
+
+	fmt.Print("Enter the expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
-	fmt.Println("Enter the number of years: ")
+
+	fmt.Print("Enter the number of years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationrate/100, years)
+
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
 }
