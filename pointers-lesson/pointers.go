226912baf0d64ea763b6269cda6fd745f8aba5a1
rolcho <roland@cimem.hu>
2024-02-10 19:37:32 +0000

    without pointers

--- pointers-lesson/pointers.go
+++ pointers-lesson/pointers.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	age := 32
+
+	adultYears := getAdultYears(age)
+	fmt.Println("Adult years: ", adultYears)
+}
+
+func getAdultYears(age int) int {
+	return age - 18
 }
