44bd1bf7d4f4bfaa6f13b2fadb9113b8351c3179
rolcho <roland@cimem.hu>
2024-02-10 06:00:16 +0000

    banking app main menu

---
+++ basics/04-banking-app/bank.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to GoBank!")
+	fmt.Println("What would you like to do today?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Enter your choice: ")
+	fmt.Scanln(&choice)
+
+	fmt.Println("You selected:", choice)
+}
