ce28323ea40b21a4b5fbd166d7bc89d7a65266b6
rolcho <roland@cimem.hu>
2024-02-10 07:56:18 +0000

    basic error handling

--- basics/04-banking-app/bank.go
+++ basics/04-banking-app/bank.go
@@ -1,19 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalance = "balance.txt"
+
+var loadedBalance float64
 
 func main() {
-	var balance float64 = 1000
 	var isLoggedin bool = true
+
+	checkBalance()
 
 	for isLoggedin {
 		showMenu()
 		var choice int
 		fmt.Print("Enter your choice: ")
 		fmt.Scanln(&choice)
-		isLoggedin = processChoice(choice, balance)
+		isLoggedin = processChoice(choice, loadedBalance)
 	}
 
+}
+
+func setBalanceInFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalance, []byte(balanceText), 0644)
+}
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalance)
+
+	if err != nil {
+		return 1000, errors.New("failed to read file")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("damaged data")
+	}
+
+	return balance, nil
 }
 
 func showMenu() {
@@ -28,11 +60,11 @@ func showMenu() {
 func processChoice(choice int, balance float64) bool {
 	switch choice {
 	case 1:
-		checkBalance(balance)
+		loadedBalance = checkBalance()
 	case 2:
-		depositMoney(balance)
+		loadedBalance = depositMoney(balance)
 	case 3:
-		withdrawMoney(balance)
+		loadedBalance = withdrawMoney(balance)
 	case 4:
 		fmt.Println("Thank you for using GoBank!")
 		return false
@@ -42,41 +74,57 @@ func processChoice(choice int, balance float64) bool {
 	return true
 }
 
-func checkBalance(balance float64) {
+func checkBalance() float64 {
+	balance, err := getBalanceFromFile()
+
+	if err != nil {
+		showError(err)
+	}
+
 	fmt.Println("Your balance is:", balance)
+	return balance
 }
 
-func depositMoney(balance float64) {
+func depositMoney(balance float64) float64 {
 	var amount float64
 	fmt.Print("Enter the amount to deposit: ")
 	fmt.Scanln(&amount)
 
 	if amount <= 0 {
 		fmt.Println("Invalid amount!")
-		return
+		return balance
 	}
 
 	balance += amount
+	setBalanceInFile(balance)
 	fmt.Println("Deposited:", amount)
 	fmt.Println("New balance:", balance)
+
+	return balance
 }
 
-func withdrawMoney(balance float64) {
+func withdrawMoney(balance float64) float64 {
 	var amount float64
 	fmt.Print("Enter the amount to withdraw: ")
 	fmt.Scanln(&amount)
 
 	if amount <= 0 {
 		fmt.Println("Invalid amount!")
-		return
+		return balance
 	}
 
 	if amount > balance {
 		fmt.Println("Insufficient balance!")
-		return
+		return balance
 	}
 
 	balance -= amount
+	setBalanceInFile(balance)
 	fmt.Println("Withdrawn:", amount)
 	fmt.Println("New balance:", balance)
+	return balance
+}
+
+func showError(err error) {
+	fmt.Printf("ERROR:\n%v\n-------------\n", err)
 }
