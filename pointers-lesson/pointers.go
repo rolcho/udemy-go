8f342525ebcb780f3163b78546fb3a38548cd170
rolcho <roland@cimem.hu>
2024-02-10 19:47:25 +0000

    basic pointer usage

--- pointers-lesson/pointers.go
+++ pointers-lesson/pointers.go
@@ -5,10 +5,14 @@ import "fmt"
 func main() {
 	age := 32
 
-	adultYears := getAdultYears(age)
+	agePointer := &age
+	fmt.Println(agePointer)
+	fmt.Println(*agePointer)
+
+	adultYears := getAdultYears(agePointer)
 	fmt.Println("Adult years: ", adultYears)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
