9c1b54cdfe910dcc544b6dbcb936daa0a2337fcb
rolcho <roland@cimem.hu>
2024-02-12 19:33:56 +0000

    update kvp

--- maps-lesson/maps.go
+++ maps-lesson/maps.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// websites := []string{"https://google.com", "https://apple.com"}
 	websites := map[string]string{
 		"google": "https://google.com",
 		"apple":  "https://apple.com",
 	}
 	websites["microsoft"] = "https://microsoft.com"
-
 	fmt.Println(websites)
+
+	websites["microsoft"] = "https://office.com"
+	fmt.Println(websites["microsoft"])
 
 	delete(websites, "microsoft")
 	fmt.Println(websites)
