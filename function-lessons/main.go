49adda8806e4ddbfde443592e6a672c54dbeae04
rolcho <roland@cimem.hu>
2024-02-17 16:39:34 +0000

    anonymous function'

--- function-lessons/main.go
+++ function-lessons/main.go
@@ -2,36 +2,22 @@ package main
 
 import "fmt"
 
-type transformFunction func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	transformed := transformNumbers(&numbers, getTransformFunction(&numbers))
+	numbers := []int{1, 2, 3}
+
+	transformed := transformNumbers(&numbers, func(n int) int {
+		return n * 2
+	})
+
 	fmt.Println(transformed)
-
-	transformed = transformNumbers(&numbers, getTransformFunction(&numbers))
-	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transformer transformFunction) []int {
-	var transformed []int
-	for _, number := range *numbers {
-		transformed = append(transformed, transformer(number))
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
 	}
-	return transformed
-}
 
-func getTransformFunction(numbers *[]int) transformFunction {
-	if (*numbers)[0] == 1 {
-		return double
-	}
-	return triple
-}
-
-func double(x int) int {
-	return x * 2
-}
-
-func triple(x int) int {
-	return x * 3
+	return dNumbers
 }
