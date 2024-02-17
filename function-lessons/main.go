4943cbf91fd7c363ce952eb098dfde186f9d1d06
rolcho <roland@cimem.hu>
2024-02-17 16:56:46 +0000

    recursion

--- function-lessons/main.go
+++ function-lessons/main.go
@@ -3,36 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3}
 
-	double := createTransformer(2)
-	triple := createTransformer(3)
-
-	transformed := transformNumbers(&numbers, func(n int) int {
-		return n * 2
-	})
-
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-
-	fmt.Println(transformed)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fact := factorial(6)
+	fmt.Println(fact)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
+func factorial(n int) int {
+	if n > 1 {
+		return n * factorial(n-1)
 	}
-
-	return dNumbers
-}
-
-// Factory function pattern
-func createTransformer(factor int) func(int) int {
-	return func(i int) int {
-		return i * factor
-	}
+	return 1
 }
