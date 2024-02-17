cee8fbc71d3c27fcd40196c5d04f3092bcdbaeca
rolcho <roland@cimem.hu>
2024-02-17 17:18:10 +0000

    spread slice

--- function-lessons/main.go
+++ function-lessons/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	s := sum(1, 5, 7, 12)
+	numbers := []int{1, 10, 15}
 
+	s := sum(10, 1, 5, 7, 12)
 	fmt.Println(s)
+
+	anotherSum := sum(1, numbers...)
+	fmt.Println(anotherSum)
 }
 
-func sum(numbers ...int) int {
-	s := 0
+func sum(startingValue int, numbers ...int) int {
+	s := startingValue
 
 	for _, n := range numbers {
 		s += n
