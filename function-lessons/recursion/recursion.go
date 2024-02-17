4fcbe6c4f0cf270bc0ffd8ea54aee6dc880ad7fa
rolcho <roland@cimem.hu>
2024-02-17 17:02:06 +0000

    recursion package

---
+++ function-lessons/recursion/recursion.go
@@ -0,0 +1,17 @@
+package recursion
+
+import "fmt"
+
+func main() {
+
+	fact := factorial(6)
+	fmt.Println(fact)
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	return n * factorial(n-1)
+}
