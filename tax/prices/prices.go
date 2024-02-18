1ca065636908c3ee778b28f9f895b10d9650750d
rolcho <roland@cimem.hu>
2024-02-18 09:29:19 +0000

    read prices from file

--- tax/prices/prices.go
+++ tax/prices/prices.go
@@ -3,7 +3,9 @@ package prices
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 type TaxIncludedPriceJob struct {
@@ -12,30 +14,62 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) LoadData() {
-	if file, err := os.Open("prices.txt"); err != nil {
-		fmt.Println("Something went wrong")
-		fmt.Println(err)
-	} else {
-		scanner := bufio.NewScanner(file)
-		var lines []string
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
+func (job *TaxIncludedPriceJob) LoadPrices() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Printf("ERROR %v\n", err)
+		return
 	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Printf("ERROR %v\n", err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Printf("ERROR %v\n", err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
+	file.Close()
+
 }
 
-func (job TaxIncludedPriceJob) Process() {
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadPrices()
+
 	result := make(map[string]float64)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		textIncludedPrice := price * (1 + job.TaxRate)
+		textIncludedPrice = math.Round(textIncludedPrice*100) / 100
+		result[fmt.Sprintf("%.2f", price)] = textIncludedPrice
 	}
 	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		InputPrices: []float64{10, 20, 30},
+		InputPrices: []float64{},
 		TaxRate:     taxRate,
 	}
 }
